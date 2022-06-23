6dfbf33ae72b6e03feef35e1f523f7a6fc8ea76b
Ronaldo Ribeiro <ronaldoribeiro_123@hotmail.com>
2022-06-22 23:25:48 -0300

    add primeiro commit Node

---
+++ Servidor express/index.js
@@ -0,0 +1,108 @@
+/*const express = require("express")
+
+
+const app = express()
+
+
+const porta = 3002
+app.listen(3002, function () {
+    console.log(`servidor rodando na porta ${porta}`);
+})
+                        // req = todos os dados da requisição vem do font end vai estar no reque
+                        // resp = todos os dados que eu mando de volta para o font end
+
+                        app.get("/livros", function(req, resp){
+    resp.end(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Meus livros favoritos </h1>
+                </body>
+            </html>
+        `       
+    )
+
+})
+//O módulo express retorna uma função que instancia o express
+const express = require("express")
+
+//A função express cria uma instância de todo o framework express em app
+const app = express()
+
+const porta = 3001
+app.listen(porta, function () {
+    console.log(`Servidor rodando na porta ${porta}`);
+})
+
+app.get("/livros", function (req, resp) {
+
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Meus livros favoritos </h1>
+                </body>
+            </html>
+        `
+    )
+})
+
+app.get("/hora", function (req, resp) {
+    let horaCerta = new Date()
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Hora certa ${horaCerta} </h1>
+                </body>
+            </html>
+        `
+    )
+})
+*/
+
+app.get("/usuarios", function (req, resp) {
+
+    //const users = ['Felisberto', 'Albertina', 'Annabella']
+
+    const user = {
+        nome: "Ivan", 
+        cpf: "000.000.000-12", 
+        email: "ivan.borchardt.cobol@gmail.com"
+    }
+    resp.json(user)
+
+})
+
+app.get("/sobre", function (req, resp) {
+
+    resp.sendFile(__dirname + "/html/sobre.html")
+
+})
+
+//Rota dinâmica
+app.get("/ws/:cep", function (req, resp) {
+
+    var cep = req.params.cep 
+    console.log(cep);
+    resp.json({cep})
+
+})
+
+//Rota dinâmica
+app.get("/endereco/:cep", function (req, resp) {
+
+    var cep = req.params.cep 
+
+    resp.json({cep})
+
+})
